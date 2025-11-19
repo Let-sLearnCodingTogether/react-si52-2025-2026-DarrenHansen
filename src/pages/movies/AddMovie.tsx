bf947e9a257676b662dd19ca57673b555a075a10
Jesen <120288897+iniJesen@users.noreply.github.com>
2025-11-19 19:15:55 +0700

    ef

--- src/pages/movies/AddMovie.tsx
+++ src/pages/movies/AddMovie.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router"
 function AddMovies() {
     return <div>
         <h2>Add Movie Page</h2>
-        <NavLink to="/">Nigger</NavLink>
+        <NavLink to="/" className="btn btn-primary">Movie</NavLink>
     </div>
 }
 export default AddMovies
--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,10 +1,35 @@
+import { useCallback, useEffect, useState } from "react"
 import { NavLink } from "react-router"
+import ApiClient from "../../utils/ApiClient"
 
-function Movies() {
-    return <div>
-        <h2>Movie Page</h2>
-        <NavLink to="/add-movie">Add Movie</NavLink>
-    </div>
+interface Movie {
+    _id : string,
+    judul : string,
+    taunRilis : string,
+    sutradara : string,
+    createdAt : string,
+    updatedAt : string
 }
 
-export default Movies;
+
+function Movies() {
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    const fetchMovies = useCallback(async () => {
+        const response = await ApiClient.get("/movie")
+
+        if(response.status == 200) {
+            setMovies(response.data.data)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchMovies()
+    }, [fetchMovies])
+
+    return <div className ="d-flex justify-content-between-mb-3">
+        <h2>Movie Page</h2>
+        <NavLink to="/add-movie" className="btn btn-primary">Add Movies</NavLink>
+    </div>
+}
+export default Movies
