bbafd9f00ecdae42cdc8991d4cd9a54c50da5243
DarrenHansen <darrenhansen_2327240163@mhs.mdp.ac.id>
2025-12-03 20:03:27 +0700

    awd

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,29 +1,44 @@
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        children : [
-            {
-                index : true,
-                lazy : {
-                    Component : async() => {
-                        const component = await import("../pages/movies/Movies.tsx")
-                        return component.default
-                    }
-                }
-            },
-            {
-                path : "add-movie",
-                lazy : {
-                    Component : async() => {
-                        const component = await import("../pages/movies/AddMovie.tsx")
-                        return component.default
-                    }
-                } 
-            }
-        ] // alamat dari sebuah page
-    }
-])
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        lazy: async () => {
+          const component = await import("../pages/movies/auth/signup/SignUp.tsx");
+          return { Component: component.default };
+        }
+      },
+      {
+        path: "signIn",
+        lazy: async () => {
+          const component = await import("../pages/movies/auth/signin/SignIn.tsx");
+          return { Component: component.default };
+        }
+      }
+    ]
+  },
+  {
+    path: "/movies",
+    children: [
+      {
+        index: true,
+        lazy: async () => {
+          const component = await import("../pages/movies/Movies.tsx");
+          return { Component: component.default };
+        }
+      },
+      {
+        path: "add-movie",
+        lazy: async () => {
+          const component = await import("../pages/movies/AddMovie.tsx");
+          return { Component: component.default };
+        }
+      }
+    ]
+  }
+]);
 
-export default router
+export default router;
--- src/utils/ApiClient.ts
+++ src/utils/ApiClient.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const ApiClient = axios.create({
     baseURL : "http://localhost:3000/api",
         headers : {
-            "Accept" : "application/json"
+            "Accept" : "application/json",
+            "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjkyNmI1YTNhMmY5ZmFiYWVmNTE0MDE0IiwidXNlcm5hbWUiOiJEYXJyZW4iLCJpYXQiOjE3NjQ3NjU3NjUsImV4cCI6MTc2NDc2NjY2NX0.4Gwbe9uGYxJyVXaixOwFgRaZIHPFGRIiGWoe-K8NxuI"
         }
 })
 
